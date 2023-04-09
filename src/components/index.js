f999f8a27db96c5e0de9da389ef6bc0eb7d50183
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-09 11:22:15 +0530

    changed file format from js to jsx

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Stats } from "./Stats";
+export { default as Business } from "./Business";
+export { default as Billing } from "./Billing";
+export { default as CardDeal } from "./CardDeal";
+export { default as Testimonials } from "./Testimonials";
+export { default as Clients } from "./Clients";
+export { default as CTA } from "./CTA";
+export { default as Footer } from "./Footer";
+export { default as NavBar } from "./NavBar";
