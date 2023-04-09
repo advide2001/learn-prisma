5d614d67d549cd8b26041e7c33fa759e56eede02
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-09 16:52:54 +0530

    removed some commented code

--- src/App.jsx
+++ src/App.jsx
@@ -20,23 +20,3 @@ const App = () => {
 };
 
 export default App;
-
-{
-  /* <div className="flex items-start justify-center bg-primary">
-        <div className="w-full xl:max-w-[1280px]">
-          <Hero />
-        </div>
-      </div>
-      <div className="flex items-start justify-center bg-primary px-6 sm:px-16">
-        <div className="w-full xl:max-w-[1280px]">
-          <Stats />
-          <Business />
-          <Billing />
-          <CardDeal />
-          <Testimonials />
-          <Clients />
-          <CTA />
-          <Footer />
-        </div>
-      </div> */
-}
